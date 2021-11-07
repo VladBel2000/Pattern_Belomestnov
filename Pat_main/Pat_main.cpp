124282517cc2d8845e2bf6a090236ad00a02ec44
VladBel2000 <93825531+VladBel2000@users.noreply.github.com>
2021-11-07 19:27:13 +0700

    7.11.21

--- Pat_main/Pat_main.cpp
+++ Pat_main/Pat_main.cpp
@@ -5,38 +5,9 @@
 #include "Speed.h"
 #include "Proxy_Device.h"
 #include "Headers.h"
+#include "Test_for_Lab.h"
 
-void test_delegate()
-{
-	printf("Test delegate:\n");
-
-	Real_Device* dv1 = new Real_Device("Auto");
-	dv1->get_rez();
-
-	Real_Device* dv2 = new Real_Device("Walk");
-	dv2->get_rez();
-
-	Real_Device* dv3 = new Real_Device("Bicycle");
-	dv3->get_rez();
-}
-void testProxy()
-{ 
-	Real_Device* dv1 = new Real_Device("Walk");
-	printf("\nDevice: "); 
-	dv1->get_rez(); 
-
-	Proxy_Device* proxyDevice = new Proxy_Device();
-	printf("Proxy device: "); 
-	proxyDevice->get_rez();
-
-
-	proxyDevice->initDevice(dv1); 
-	printf("Device: ");
-	dv1->get_rez(); 
-	printf("Proxy device: ");
-	proxyDevice->get_rez();
-}
 int main()
 {
-	testProxy();
+	test_Iterator();
 }
